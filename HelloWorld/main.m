b7683e33a90e1e1dce416a4ff73723ffb9bb4f1c
Jacob <jacob@JacobdeMac.local>
2014-01-03 20:07:35 +0800

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Jacob on 14-1-3.
+//  Copyright (c) 2014年 Jacob. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
